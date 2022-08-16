5c5714735d14746f9f1d382af12a924a59d1e9c8
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-08-16 21:08:29 +0200

    Use Official Secure RNG, Allow multiple frameworks

---
+++ Curve25519.NetCore.Test/Curve25519_Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Curve25519.NetCore.Examples;
+
+internal class Curve25519_Tests
+{
+    [Test]
+    public void TestEquality()
+    {
+        var curve25519   = new Curve25519();
+        
+        var alicePrivate = curve25519.CreateRandomPrivateKey();
+        var alicePublic  = curve25519.GetPublicKey(alicePrivate);
+        
+        Assert.False(alicePrivate.SequenceEqual(alicePublic));
+        
+        var bobPrivate = curve25519.CreateRandomPrivateKey();
+        var bobPublic  = curve25519.GetPublicKey(bobPrivate);
+        
+        Assert.False(bobPrivate.SequenceEqual(bobPublic));
+        Assert.False(alicePrivate.SequenceEqual(bobPrivate));
+        
+        var aliceShared = curve25519.GetSharedSecret(alicePrivate, bobPublic);
+        var bobShared   = curve25519.GetSharedSecret(bobPrivate,   alicePublic);
+        
+        Assert.True(aliceShared.SequenceEqual(bobShared));
+    }
+}
